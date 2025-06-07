0deb369312badf614e81b54d97479168ece562ad
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-06-07 18:28:15 +0300

    Update 3Sum Closest.cpp

--- 3Sum Closest.cpp
+++ 3Sum Closest.cpp
@@ -3,21 +3,20 @@ public:
     int threeSumClosest(vector<int>& v, int t) {
         int n=v.size();
         sort(v.begin(),v.end());
-        int mn=INT_MAX;
-        bool f=1;
+       int close=v[0]+v[1]+v[2];
         for(int i=0;i<n-2;i++){
         if(i>0&&v[i]==v[i-1])continue;
         int l=i+1;
         int r=n-1;
         while(l<r)
         {
-            int sum=v[i]+v[l]+v[r];
-            if(abs(t-sum)<=mn){if(t-sum<0)f=0;else f=1;mn=abs(t-sum);}
-           
-          if(t-sum>sum-t)l++;
-            else r--;
+            long sum=v[l]+v[r]+v[i];
+            if(abs(t-sum)<abs(t-close))close=sum;
+            if(sum<t)l++;
+            else if(sum>t)r--;
+            else return t;
         }
         }
-       if(f)return t-mn;else return t+mn;
+      return close;
     }
 };
